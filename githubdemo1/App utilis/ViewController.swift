9cf16e3dbf4a6e6359137917cb6c03add027a20e
shokh <1mustafoman@gmail.com>
2022-03-19 18:45:47 +0500

    Initial Commit

---
+++ githubdemo1/App utilis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubdemo1
+//
+//  Created by Shohjahon Numonovich on 3/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
